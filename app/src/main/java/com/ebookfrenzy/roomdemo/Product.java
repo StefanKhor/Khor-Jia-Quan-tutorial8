d2e1ff3e05bd9fb1911afe7b3aed4dd8e79503c2
Stefan <stefankhor2002@gmail.com>
2024-06-04 09:31:01 +0800

    67.3  Building Entity - Define Product Class, Getter and Setter

--- app/src/main/java/com/ebookfrenzy/roomdemo/Product.java
+++ app/src/main/java/com/ebookfrenzy/roomdemo/Product.java
@@ -1,5 +1,10 @@
 package com.ebookfrenzy.roomdemo;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey
+
 public class Product {
     private int id;
     private String name;
@@ -15,8 +20,23 @@ public class Product {
     }
 
     public String getName(){
+        return this.name;
+    }
 
-        return null;
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
     }
 }
 
