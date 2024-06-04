6e7b4f60d1c460b4ca364b6f7c10aafd32b3ff98
Stefan <stefankhor2002@gmail.com>
2024-06-04 09:07:38 +0800

    66.2 Created Project 66.3 Converted to LinearLayout 66.4 Add TableLayout to User Interface 66.5 Configure TableRows 66.6 Add Button Bar to Layout 66.7 Add RecyclerView 66.8 Adjust Layout Margin

---
+++ app/src/main/java/com/ebookfrenzy/roomdemo/Product.java
@@ -0,0 +1,22 @@
+package com.ebookfrenzy.roomdemo;
+
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Product(String name, int quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public String getName(){
+
+        return null;
+    }
+}
+
